1975531225b132e212a348c47955c66c62ad0ba4
Everton Granado Silva <granado2309@gmail.com>
2022-07-27 07:12:18 -0300

    initial Commit

---
+++ aula3.py
@@ -0,0 +1,9 @@
+nome = input('Qual é o seu nome:  ')
+print('É um prazer te conhecer, {}!'.format(nome))
+
+
+
+
+
+
+
---
+++ ex004.py
@@ -0,0 +1,10 @@
+a = input( 'Digite algo: ')
+print('O tipo primitivo desse valor é', type(a))
+print('Só tem espaços ?', a.isspace())
+print('Ele é alphanúmerico ?',a.isalnum())
+print('Ele é alfabético ?', a.isalpha())
+print('É um número ?', a.isnumeric())
+print('Esta em maiusculas ?',a.isupper())
+print('Esta em minusculas ?', a.islower())
+print('Esta capitalizada',a.istitle())
+
