5c6f16502bbcf6a105edd746d3cc4ecc83815120
Everton Granado Silva <granado2309@gmail.com>
2022-07-29 13:02:46 -0300

    initial Commit

--- ex009.py
+++ ex009.py
@@ -16,17 +16,35 @@
 #t = n*1,n*2,n*3,n*4,n*5,n*6,n*7,n*8,n*9,n*10
 #print(f'A tabuada do número {n} do 1 ao 10 respectivamente é: {t}')
 
-num = int(input('Digite um número para ver sua tabuada: '))
-print('{} x {:2} = {}' .format(num,1,num*1))
-print('{} x {:2} = {}' .format(num,2, num*2))
-print('{} x {:2} = {}' .format(num, 3,num*3))
-print('{} x {:2} = {}' .format(num, 4,num*4))
-print('{} x {:2} = {}' .format(num, 5,num*5))
-print('{} x {:2} = {}' .format(num, 6,num*6))
-print('{} x {:2} = {}' .format(num, 7,num*7))
-print('{} x {:2} = {}' .format(num, 8,num*8))
-print('{} x {:2} = {}' .format(num, 9,num*9))
-print('{} x {:2} = {}' .format(num, 10,num*10))
+
+from os import system ,startfile
+from time import sleep
+
+            opcao =1
+            while opcao != 0:
+                   print('')
+                   print('Exercícios')
+                   print('')
+                   print('9) Tabuada')
+                   opcao = int(input('Dígite a opção desajada, ou zero para sair:'))
+
+                   if opcao==9:
+                                      #Exercício tabuada
+
+                        num = int(input('Digite um número para ver sua tabuada: '))
+                        print('-'* 12)
+                        print('{} x {:2} = {}' .format(num,1,num*1))
+                        print('{} x {:2} = {}' .format(num,2, num*2))
+                        print('{} x {:2} = {}' .format(num, 3,num*3))
+                        print('{} x {:2} = {}' .format(num, 4,num*4))
+                        print('{} x {:2} = {}' .format(num, 5,num*5))
+                        print('{} x {:2} = {}' .format(num, 6,num*6))
+                        print('{} x {:2} = {}' .format(num, 7,num*7))
+                        print('{} x {:2} = {}' .format(num, 8,num*8))
+                        print('{} x {:2} = {}' .format(num, 9,num*9))
+                        print('{} x {:2} = {}' .format(num, 10,num*10))
+                        print('-'*12)
+
 
 
 
